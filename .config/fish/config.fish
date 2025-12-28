97399805e3694c7b26f9eca562fcee1ebb552033
Jayesh <jayeshvegda198@gmail.com>
2025-12-28 16:31:23 +0530

    Setting up Terminal more better

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,35 +1,42 @@
-starship init fish | source
-set -g fish_greeting
+## 1. GLOBAL SETTINGS
+#set -g fish_greeting "" # Disable the default greeting
+set -gx LANG en_US.UTF-8
 
-alias cfg="nano ~/.config/hypr/hyprland.conf"
-alias cursor="cursor --ozone-platform-hint=wayland"
+function fish_greeting
+    fastfetch
+end
+# Fix for Ghostty/Kit/Alacritty SSH issues
+# This forces SSH to use a standard terminal type so remote servers don't break
+#alias ssh="TERM=xterm-256color /usr/bin/ssh"
 
+## 2. INTERACTIVE SESSION (Only run this for the user, not scripts)
 if status is-interactive
-    # Initialize Starship (The prompt you just installed)
+
+    # --- TOOLS INIT ---
     starship init fish | source
 
-    # Initialize Zoxide (The smart cd)
-    zoxide init fish | source
+    # Initialize Zoxide and replace 'cd' automatically
+    # This removes the need for 'alias cd=z'
+    zoxide init fish --cmd cd | source
 
-    # ~ ALIASES ~
-    # Replace 'ls' with 'eza' (icons + git status)
+    # --- ABBREVIATIONS (Better than aliases) ---
+    # Expands when you type. e.g. 'cfg' -> space -> expands to full path
+    abbr --add cfg "nvim ~/.config/hypr/hyprland.conf"
+    abbr --add lzd lazydocker
+
+    # --- ALIASES ---
+    # Visuals
     alias ls="eza --icons --group-directories-first"
     alias ll="eza -l --icons --group-directories-first --git"
-
-    # Replace 'cat' with 'bat' (colors)
     alias cat="bat"
 
-    # Replace 'cd' with 'z' (so you don't have to learn a new key)
-    alias cd="z"
-end
-# Make Ctrl+V behave like normal paste
-# Disable fzf variable search completely
-bind -M insert -e ctrl-v
-bind -M insert \cv paste
-# --- Disable fzf.fish variable search completely
-set -g FZF_DISABLE_VARIABLES 1
+    # Hyprland & Wayland specific
+    alias cursor="cursor --ozone-platform-hint=wayland"
 
-# --- JAY'S SERVER TOOLS ---
-alias lzd='lazydocker'
-alias tv-control='qtscrcpy'
-set -gx LANG en_US.UTF-8
+    # --- VI MODE & KEYBINDINGS ---
+    set -g fish_key_bindings fish_vi_key_bindings
+end
+
+# Remove stty commands unless strictly necessary (can slow down startup)
+# stty -ixon
+# stty sane
